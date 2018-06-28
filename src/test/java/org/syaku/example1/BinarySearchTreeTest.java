6fc9cc23364ad4138cad37b6e48c2415cc2b0119
최석균 <syaku@naver.com>
2018-06-28 23:31:59 +0900

    맵형식 테스트 추가

---
+++ src/test/java/org/syaku/example1/BinarySearchTreeTest.java
@@ -0,0 +1,8 @@
+package org.syaku.example1;
+
+/**
+ * @author Seok Kyun. Choi. 최석균 (Syaku)
+ * @since 2018. 6. 28.
+ */
+public class BinarySearchTreeTest {
+}
--- src/test/java/org/syaku/example1/StringBinarySearchTest.java
+++ src/test/java/org/syaku/example1/StringBinarySearchTest.java
@@ -1,7 +1,9 @@
 package org.syaku.example1;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class StringBinarySearchTest {
   List<Contacts> result = new ArrayList<>();
+  long start = 0;
 
   @Before
   public void setup() {
@@ -25,24 +28,36 @@ public class StringBinarySearchTest {
     for (int i = 0; i < 1000000; i++) {
       result.add(new Contacts(UUID.randomUUID().toString(), "010"));
     }
+    start = System.currentTimeMillis();
   }
-  @Test(timeout = 1000)
-  public void 직접만든기능이용() {
-    long start = System.currentTimeMillis();
+
+  @After
+  public void exit() {
+    log.debug("exit -> {} ms", (System.currentTimeMillis() - this.start));
+  }
+
+  @Test
+  public void 직접개발() {
     Collections.sort(result);
     Assert.assertEquals(Contacts.findByName(result, "가나다"), new Contacts("가나다", "010"));
     Assert.assertEquals(Contacts.findByName(result, "홍길동"), new Contacts("홍길동", "010"));
     Assert.assertNull(Contacts.findByName(result, "아무개"));
-    log.debug("exit -> {} ms", (System.currentTimeMillis() - start));
   }
 
-  @Test(timeout = 1000)
-  public void 자바컬랙션이용() {
-    long start = System.currentTimeMillis();
+  @Test
+  public void 자바컬랙션() {
     Collections.sort(result);
     Assert.assertEquals(result.get(Collections.binarySearch(result, new Contacts("가나다", "010"))), new Contacts("가나다", "010"));
     Assert.assertEquals(result.get(Collections.binarySearch(result, new Contacts("홍길동", "010"))), new Contacts("홍길동", "010"));
     Assert.assertTrue(Collections.binarySearch(result, new Contacts("아무개", "010")) < 0);
-    log.debug("exit -> {} ms", (System.currentTimeMillis() - start));
+
+  }
+
+  @Test
+  public void 맵() {
+    Map<String, Contacts> data = result.stream().collect(Collectors.toMap(Contacts::getName, contacts -> contacts));
+    Assert.assertEquals(data.get("가나다"), new Contacts("가나다", "010"));
+    Assert.assertEquals(data.get("홍길동"), new Contacts("홍길동", "010"));
+    Assert.assertNull(data.get("아무개"));
   }
 }
